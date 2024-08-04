c130d6122c824bd98d81630ef74b68654cfa42e7
Shahoriarnahid <shahoriar632@gmail.com>
2024-08-04 17:02:56 +0600

    update admin page design

--- lib/pages/admin_page.dart
+++ lib/pages/admin_page.dart
@@ -1,3 +1,4 @@
+import 'package:assessment/helpers/k_text.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../base/base.dart';
@@ -16,57 +17,73 @@ class _AdminPageState extends State<AdminPage> {
       appBar: AppBar(
         title: const Text('Admin'),
       ),
-      body: Column(
-        children: [
-          const Expanded(
-            child: Center(
-              child: Text('This is the user page to capture screenshots from.'),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Center(
+              child: KText(
+                text: 'This is the Admin page to Ability that capture screenshots at adjustable intervals & show the screenshots.',
+                maxLines: 3,
+                textAlign: TextAlign.justify,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  // Base.screenCaptureController.isCapturing.value
-                  //     ? Base.screenCaptureController.stopCapturing
-                  //     : Base.screenCaptureController.startCapturing,
-                  child: Obx(() => Text(Base.screenCaptureController.isCapturing.value ? 'Stop' : 'Start')),
-                ),
-                SizedBox(width: 10),
-                DropdownButton<int>(
-                  value: Base.screenCaptureController.interval.value,
-                  items: [5, 10, 15, 20, 30]
-                      .map((e) => DropdownMenuItem<int>(
-                            value: e,
-                            child: Text('$e seconds'),
-                          ))
-                      .toList(),
-                  onChanged: (value) {
-                    if (value != null) {
-                      Base.screenCaptureController.setInterval(value);
-                    }
-                  },
-                ),
+                KText(text: 'Time Interval: '),
+                const SizedBox(width: 10),
+                Obx(() => DropdownButton<int>(
+                      value: Base.screenCaptureController.interval.value,
+                      items: [5, 10, 15, 20, 30]
+                          .map((e) => DropdownMenuItem<int>(
+                                value: e,
+                                child: Text('$e seconds'),
+                              ))
+                          .toList(),
+                      onChanged: (value) {
+                        if (value != null) {
+                          Base.screenCaptureController.setInterval(value);
+                        }
+                      },
+                    )),
               ],
             ),
-          ),
-          Expanded(
-            child: Obx(() {
-              return ListView.builder(
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, // Number of columns
+                  crossAxisSpacing: 10, // Spacing between columns
+                  mainAxisSpacing: 10, // Spacing between rows
+                ),
                 itemCount: Base.screenCaptureController.screenshots.length,
                 itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.memory(Base.screenCaptureController.screenshots[index]),
+                  return GridTile(
+                    child: Container(
+                      color: Colors.white,
+                      child: Center(
+                        child: Image.memory(Base.screenCaptureController.screenshots[index]),
+                      ),
+                    ),
                   );
                 },
-              );
-            }),
-          ),
-        ],
+
+                // Expanded(
+                //   child: Obx(() {
+                //     return ListView.builder(
+                //       itemCount: Base.screenCaptureController.screenshots.length,
+                //       itemBuilder: (context, index) {
+                //         return Padding(
+                //           padding: const EdgeInsets.all(8.0),
+                //           child: Image.memory(Base.screenCaptureController.screenshots[index]),
+                //         );
+                //       },
+                //     );
+                //   }),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
